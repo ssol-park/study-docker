7ac930cbbd93a4cdeb051aedec46318c5362765f
ssol-park <ksttlov.naver@hanmail.net>
2022-11-20 13:48:59 +0900

    initial commit

---
+++ src/main/java/com/study/docker/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.study.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "test";
+    }
+
+}
